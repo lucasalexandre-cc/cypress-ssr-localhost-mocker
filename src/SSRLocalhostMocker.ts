29d40ee40cc0547bc9ae36c128845c295e468438
Lucas Alexandre <lucasalexandre.s.cc@gmail.com>
2023-10-07 16:11:57 -0300

    feat(SSRLocalhostMocker#init): if servers already exists, check if ports param are equals

--- src/SSRLocalhostMocker.ts
+++ src/SSRLocalhostMocker.ts
@@ -7,8 +7,17 @@ export default class SSRLocalhostMocker implements ISSRLocalhostMocker {
   constructor(private localhostServerFactory: ILocalhostServerFactory) {}
 
   async init(...ports: number[]): Promise<void> {
+    this.validatePorts(ports);
+
     if (!this.servers) {
       this.servers = ports.map((port) => this.localhostServerFactory.create(port));
+    }
+  }
+
+  private validatePorts(ports: number[]): void {
+    if (this.servers) {
+      const portsAreDifferent = this.servers.some((server) => !ports.includes(server.getPort()));
+      if (portsAreDifferent) throw new Error('SSRLocalhostMocker: trying to init with different ports');
     }
   }
 }
