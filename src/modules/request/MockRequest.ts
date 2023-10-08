c0bbd93e001646d704965dff00005163f855d77c
Lucas Alexandre <lucasalexandre.s.cc@gmail.com>
2023-10-08 14:34:26 -0300

    feat(MockRequest#matchRequest): return false if path does not match

--- src/modules/request/MockRequest.ts
+++ src/modules/request/MockRequest.ts
@@ -1,19 +1,24 @@
 import { IRequestInfo } from '../../types';
+import { IMatchPath } from '../../utils/types';
 import { IRequest, IResponse } from '../server/types';
 import { IMockRequest } from './types';
 
 export default class MockRequest implements IMockRequest {
   private requestInfo: IRequestInfo;
+  private matchPath: IMatchPath;
 
-  constructor(requestInfo: IRequestInfo) {
+  constructor(requestInfo: IRequestInfo, matchPath: IMatchPath) {
     this.requestInfo = requestInfo;
+    this.matchPath = matchPath;
   }
 
   matchRequest(request: IRequest): boolean {
     if (this.requestInfo.method !== request.method) return false;
+    if (!this.matchPath.match(this.requestInfo.path, request.path)) return false;
 
     return true;
   }
+
   handleResponse(response: IResponse): void {
     // TODO: implement
   }
